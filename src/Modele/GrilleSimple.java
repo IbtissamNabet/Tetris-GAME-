0223466bd088a1684cc58d90532f8241352b1d10
p2208660 <ibtissam.nabet@etu.univ-lyon1.fr>
2023-11-23 23:24:04 +0100

    score

--- src/Modele/GrilleSimple.java
+++ src/Modele/GrilleSimple.java
@@ -10,18 +10,18 @@ public class GrilleSimple extends Observable implements Runnable {
 
     //initialisation de la piéce
     private Piece pieceCourante;
+    int score;
 
 
     public GrilleSimple() {
         grille = new int[TAILLE][TAILLE];
         initialiser();
 
-        pieceCourante=genererPieceAleatoire();
+        pieceCourante = genererPieceAleatoire();
         System.out.println("Coordonnées initiales de la pièce : x = " + pieceCourante.getx() + ", y = " + pieceCourante.gety());
         new OrdonnanceurSimple(this).start(); // pour changer le temps de pause, garder la référence de l'ordonnanceur
 
     }
-
 
 
     private void initialiser() {
@@ -32,6 +32,7 @@ public class GrilleSimple extends Observable implements Runnable {
         }
 
     }
+
     public Piece genererPieceAleatoire() {
         return new Piece(this, Formes.values()[(int) (Math.random() * Formes.values().length)]);
         //return new Piece(this, Piece.Formes.O);
@@ -55,8 +56,8 @@ public class GrilleSimple extends Observable implements Runnable {
     }
 
     public void placerCase(int x, int y, int codeCouleur) {
-      //  if (validationPosition(x, y)) {
-            grille[y][x] = codeCouleur;
+        //  if (validationPosition(x, y)) {
+        grille[y][x] = codeCouleur;
     }
     //}
 
@@ -66,16 +67,24 @@ public class GrilleSimple extends Observable implements Runnable {
 
     }
 
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int s) {
+        score = s;
+    }
+
     public boolean validationPosition(int _nextX, int _nextY) {
         return (_nextX >= 0 && _nextX < TAILLE && _nextY >= 0 && _nextY < TAILLE);
     }
 
     public void run() {
-        if(pieceCourante.blocked){
+        if (pieceCourante.blocked) {
             System.out.println("placer piece");
             placerPiece(pieceCourante);
             verifGrille();
-            pieceCourante=genererPieceAleatoire();
+            pieceCourante = genererPieceAleatoire();
         }
         pieceCourante.run();
         setChanged();
@@ -85,27 +94,28 @@ public class GrilleSimple extends Observable implements Runnable {
     public Piece getPieceCourante() {
         return pieceCourante;
     }
-    public void setPieceCourante(Piece p ){
-        this.pieceCourante=p;
+
+    public void setPieceCourante(Piece p) {
+        this.pieceCourante = p;
     }
 
-    public int[][] getGrille(){
+    public int[][] getGrille() {
         return this.grille;
     }
     //verifier la colision dans la grille
     //verifier la collision avec la grille inferieure
 
-//verifier si ya collision de la piece p dans la posotion px py dans la grille
-    public boolean verifColision(Piece p,int px ,int py ) {
+    //verifier si ya collision de la piece p dans la posotion px py dans la grille
+    public boolean verifColision(Piece p, int px, int py) {
         //px et py  les coordonnées de la pièce par rapport à la grille.
         //on recupere le tableau qui represente le type de la piece actuelle
         int[][] type = p.getType();
         for (int y = 0; y < type.length; y++) {
-            for (int  x= 0; x < type[y].length; x++) {
+            for (int x = 0; x < type[y].length; x++) {
                 //on parcourt le tableau de la piece
                 //si ya pas d'autre piece (d'ou !=0) et si on est pas en bas de la gille
                 if (type[y][x] == 1) {
-                //calcul de position dans la grille
+                    //calcul de position dans la grille
                     int grilleX = x + px;
                     int grilleY = y + py;
 
@@ -114,59 +124,93 @@ public class GrilleSimple extends Observable implements Runnable {
                         return true;
                     }
                 }
-                }
             }
+        }
         return false; //pas de colision
     }
-    public void gauche(){
+
+    public void gauche() {
         pieceCourante.gauche();
 
 
     }
-    public  void droite(){
+
+    public void droite() {
         pieceCourante.droite();
 
     }
-    public  void bas (){
+
+    public void bas() {
         pieceCourante.bas();
 
 
     }
-    public  void rotation(){
+
+    public void rotation() {
         pieceCourante.rotation();
     }
 
-    public boolean ligneComplete(int y){
-        boolean complete=true;
-        for (int x=0;x<grille[y].length;x++){
-            if (grille[y][x]==0){
-                complete=false;
+    public boolean ligneComplete(int y) {
+        boolean complete = true;
+        for (int x = 0; x < grille[y].length; x++) {
+            if (grille[y][x] == 0) {
+                complete = false;
                 break;
             }
         }
         return complete;
     }
 
-    public void supprimeLigne(int L){
-        for (int y=L;y>=0;y--){
-            for(int x=0;x<grille[y].length;x++){
-                if(y==0){
-                    grille[y][x]=0;
-                }
-                else{
-                    grille[y][x]=grille[y-1][x];
+    public void supprimeLigne(int L) {
+        for (int y = L; y >= 0; y--) {
+            for (int x = 0; x < grille[y].length; x++) {
+                if (y == 0) {
+                    grille[y][x] = 0;
+                } else {
+                    grille[y][x] = grille[y - 1][x];
                 }
             }
         }
     }
 
-    public void verifGrille(){
-        for (int y=0;y<grille.length;y++){
-            if(ligneComplete(y)){
-                System.out.println("ligne "+y+" complete");
+    public void verifGrille() {
+        int nbligneSupp = 0;
+        for (int y = 0; y < grille.length; y++) {
+            if (ligneComplete(y)) {
+                System.out.println("ligne " + y + " complete");
+
                 supprimeLigne(y);
+                nbligneSupp++;
             }
         }
-    }
+        switch (nbligneSupp) {
+            case 1:
+                System.out.println("une ligne vient d'étre supprimé , score = " + score);
+                setScore(40+getScore());
+                break;
 
+            case 2:
+                System.out.println("une 2eme ligne vient d'étre supprimé , score = " + score);
+
+                setScore(100+getScore());
+                break;
+            case 3:
+                System.out.println("une 3 eme ligne vient d'étre supprimé , score = " + score);
+
+                setScore(300+getScore());
+                break;
+            case 4:
+                System.out.println("une 4 eme ligne vient d'étre supprimé , score = " + score);
+
+                setScore(1200+getScore());
+                break;
+            default:
+                setScore(0+getScore());
+
+        }
+
+    }
 }
+
+
+
--- src/VueControleur/VC.java
+++ src/VueControleur/VC.java
@@ -13,6 +13,7 @@ import java.util.concurrent.Executors;
 public class VC extends JFrame implements Observer {
 
     JTextField jt = new JTextField("");
+    JTextField jscore = new JTextField("");
     JButton jb = new JButton("do");
     GrilleSimple modele;
 
@@ -29,6 +30,8 @@ public class VC extends JFrame implements Observer {
 
         jp.add(jt, BorderLayout.NORTH);
         jp.add(jb, BorderLayout.SOUTH);
+        jp.add(jscore, BorderLayout.EAST);
+
 
         //vueGrille = new VueGrilleV1(modele); // composants swing, saccades
         vueGrille = new VueGrilleV2(modele); // composant AWT dédié
@@ -51,32 +54,6 @@ public class VC extends JFrame implements Observer {
             }
         });
 
-       /* jb.addKeyListener(new KeyAdapter() {
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                System.out.println("Key Pressed: " + e.getKeyChar());//évènement clavier : object contrôleur qui réceptionne
-                super.keyPressed(e);
-                switch (e.getKeyCode()) {
-                    case KeyEvent.VK_SPACE:
-                        modele.droite();
-                        System.out.println("touche SPACE enfoncée ");
-                        break;
-                    case KeyEvent.VK_RIGHT:
-                        modele.droite();
-                        System.out.println("touche DROITE enfoncée ");
-                        break;
-                    case KeyEvent.VK_LEFT:
-                        modele.gauche();
-                        System.out.println("touche GAUCHE enfoncée ");
-                        break;
-                    /*case KeyEvent.VK_DOWN:modele.bas();
-                        System.out.println("touche BAS enfoncée ");
-                    break;
-                }
-
-            }
-        });*/
 
         KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
         manager.addKeyEventDispatcher(new KeyEventDispatcher() {
@@ -127,6 +104,7 @@ public class VC extends JFrame implements Observer {
 
                 jt.setText("Elapsed time : " + (System.currentTimeMillis() - lastTime) + "ms - x = " + modele.getPieceCourante().getx() + " y = " + modele.getPieceCourante().gety());
                 lastTime = System.currentTimeMillis();
+                jscore.setText("Le score actuel est :"+(modele.getScore()));
 
             }
         });
